2ed34704de31ff269369951f720ea0438cd234bf
Michael Mertl <Michael.Mertl@hs-augsburg.de>
2022-04-16 17:02:08 +0200

    canvas initial

--- SnakeRift/src/pages/Playfield.tsx
+++ SnakeRift/src/pages/Playfield.tsx
@@ -1,4 +1,4 @@
-import { IonCard, IonIcon} from '@ionic/react';
+/*import { IonCard, IonIcon} from '@ionic/react';
 import { square} from "ionicons/icons";
 
 const Playfield: React.FC = () => {
@@ -14,4 +14,23 @@ const Playfield: React.FC = () => {
   );
 };
 
-export default Playfield;
+export default Playfield;*/
+
+import React, { useRef, useEffect } from 'react'
+
+const Playfield = (props: JSX.IntrinsicAttributes & React.ClassAttributes<HTMLCanvasElement> & React.CanvasHTMLAttributes<HTMLCanvasElement>) => {
+
+  const canvasRef = useRef(null);
+
+  useEffect(() => {
+    const canvas: any = canvasRef.current
+    const context = canvas.getContext('2d')
+    //Our first draw 
+    context.fillStyle = '#000000'
+    context.fillRect(0, 0, context.canvas.width, context.canvas.height)
+  }, [])
+
+  return <canvas ref={canvasRef} {...props} />
+}
+
+export default Playfield
